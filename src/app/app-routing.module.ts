5a434d3a5ef2558b5997a62b935647488ffefba7
madhu <madhusudhanan.suresh@gmail.com>
2024-02-18 08:32:07 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TopStocksTodayComponent } from './top-stocks-today/top-stocks-today.component';
 import { MarketStatisticsComponent } from './market-statistics/market-statistics.component';
 
 const routes: Routes = [
