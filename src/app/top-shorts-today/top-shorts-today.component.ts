24dec8b6ed87619fc1d8052a59f7de669ac6e4b0
madhu <madhusudhanan.suresh@gmail.com>
2024-03-03 16:43:38 -0500

    update

--- src/app/top-shorts-today/top-shorts-today.component.ts
+++ src/app/top-shorts-today/top-shorts-today.component.ts
@@ -65,7 +65,7 @@ export class TopShortsTodayComponent implements OnInit, AfterViewInit {
       this.marketStatistics$
         .pipe(
           map(data => data
-            .filter(statistic => statistic.thirtyMin && statistic.thirtyMin?.rvol > 150 && statistic.thirtyMin?.rsrw < -1)
+            .filter(statistic => statistic.thirtyMin && statistic.thirtyMin?.rvol > 150 && statistic.thirtyMin?.rsrw < -0.5)
             .sort((a, b) => b.thirtyMin.rvol - a.thirtyMin.rvol)
           )
         )
